5413c051dad9f7d26f86734b98babe6744aa5fd2
tripiodesign <j.geraldo.romero@hotmail.com>
2021-03-27 15:47:54 -0300

    Primera aplicacion con nuxt

---
+++ pages/inspire.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-row>
+    <v-col cols="12" class="text-center">
+      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+      <blockquote class="blockquote">
+        &#8220;Aplicación de práctica&#8221;
+        <v-container>
+          <p>hola mundo</p>
+        </v-container>
+        <footer>
+          <small>
+            <em>&mdash;Hola mundo</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-col>
+    <v-col>
+      <v-container>
+        <p></p>
+      </v-container>
+    </v-col>
+  </v-row>
+</template>
